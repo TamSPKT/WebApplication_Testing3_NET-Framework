5c65b6fc02fdc030c7b9f6c4f5e1d9bce8446996
TamSPKT <72260533+TamSPKT@users.noreply.github.com>
2021-04-21 21:23:08 +0700

    Add project files.

---
+++ WebApplication_Testing3_NET Framework/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication_Testing3_NET_Framework.Models;
+
+namespace WebApplication_Testing3_NET_Framework.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Users/Login/
+        public ActionResult Login()
+		{
+            return View();
+		}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (CASIOEntities db = new CASIOEntities())
+                {
+                    var obj = db.Users.Where(a => a.UserName.Equals(objUser.UserName) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.UserID.ToString();
+                        Session["UserName"] = obj.UserName.ToString();
+                        Session["UserObj"] = obj;
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                User user = (User)Session["UserObj"];
+                return View(user);
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
---
+++ WebApplication_Testing3_NET Framework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication_Testing3_NET_Framework.MvcApplication" Language="C#" %>
